05badddff771ddd8040d83b0af61b983540393a7
Andrew Myhre <andrew.myhre@gmail.com>
2011-11-22 22:08:06 +0000

    updated deployment script

--- deploythis.bat
+++ deploythis.bat
@@ -1,3 +1,3 @@
 path=%path%;c:\windows\microsoft.net\framework\v4*
 msbuild AndrewMyhre.com.2010.Web\AndrewMyhre.com.2010.Web.csproj /t:Clean;Build;Package /v:m /m:8
-deploy "AndrewMyhre.com.2010.Web\obj\Debug\Package\AndrewMyhre.com.2010.Web.zip" "http://beta.andrewmyhre.com:8172/MsDeploy2/" %1 %2 "Default Web Site"
+deploy "AndrewMyhre.com.2010.Web\obj\Debug\Package\AndrewMyhre.com.2010.Web.csproj.zip" "http://beta.andrewmyhre.com:8172/MsDeploy2/" %1 %2 "Default Web Site"
