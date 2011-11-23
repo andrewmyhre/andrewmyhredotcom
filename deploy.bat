5318844080bece85b1d7b4103435aa7cedc07856
Andrew Myhre <andrew.myhre@gmail.com>
2011-11-23 13:43:40 +0000

    fixed msdeploy path in deployment script

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@ set TargetUsername=%3
 set TargetPassword=%4
 set WebsiteName=%5
 
-"c:\program files\IIS\Microsoft Web Deploy\msdeploy.exe" -verb:sync -source:package=%PackagePath% -dest:auto,computerName=%TargetAddress%,userName=%TargetUsername%,password=%TargetPassword% -allowUntrusted -setParam:"IIS Web Application Name"=%WebsiteName%
+"c:\program files (x86)\IIS\Microsoft Web Deploy V2\msdeploy.exe" -verb:sync -source:package=%PackagePath% -dest:auto,computerName=%TargetAddress%,userName=%TargetUsername%,password=%TargetPassword% -allowUntrusted -setParam:"IIS Web Application Name"=%WebsiteName%
